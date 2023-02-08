f6840b6c9ffea0442b0a84168aadc5b1c5b3d9ac
Sarp_dg92@student.itstep.org <Sarp_dg92@student.itstep.org>
2023-02-08 17:45:55 +0200

    Add project files.

---
+++ dz_8.02/Credit.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz_8._02
+{
+    class Credit
+    {
+        public string Num_Card { get; set; }
+        public string name { get; set; }
+        public int CVC { get; set; }
+        public string Data_End { get; set; }
+
+        public Credit()
+        {
+
+        }
+        public Credit(string n_c, string n, int c, string d)
+        {
+            Num_Card = n_c;
+            name = n;
+            CVC = c;
+            Data_End = d;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Num_Card: {Num_Card};\nName: {name};\nCVC: {CVC};\nData End: {Data_End}.");
+        }
+        public override string ToString()
+        {
+            return $"Num_Card: {Num_Card};\nName: {name};\nCVC: {CVC};\nData End: {Data_End}.";
+        }
+    }
+}
---
+++ dz_8.02/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz_8._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Credit credit_card = new Credit();
+            try
+            {
+                Console.Write("Введите номер карты: ");
+                credit_card.Num_Card = Console.ReadLine();
+                if(credit_card.Num_Card.Length < 16 || credit_card.Num_Card.Length >16)
+                {
+                    throw new Exception("Номер карты должен состоять из 16 чисел!");
+                }
+                else if(credit_card.Num_Card.IndexOf(" ") != -1)
+                {
+                    throw new Exception("В номере не должно быть пробелов!");
+                }
+
+                Console.Write("Введите ФИО: ");
+                credit_card.name = Console.ReadLine();
+                if(credit_card.name == "")
+                {
+                    throw new Exception("Неправильное ФИО!");
+                }
+
+                Console.Write("Введите CVC: ");
+                credit_card.CVC = Convert.ToInt32(Console.ReadLine());
+                if(credit_card.CVC.ToString().Length < 3 || credit_card.CVC.ToString().Length > 3)
+                {
+                    throw new Exception("В CVC должно быть 3 числа!");
+                }
+
+                Console.Write("Введите срок карты: ");
+                credit_card.Data_End = Console.ReadLine();
+
+                Console.WriteLine("\n"+credit_card);
+            }
+            catch(Exception a)
+            {
+                Console.WriteLine(a.Message);
+            }
+            
+        }
+    }
+}
